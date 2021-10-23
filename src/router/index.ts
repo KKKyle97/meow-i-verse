7254afa35ff9e1362d97301a21695e0f5265667b
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-23 16:30:27 +0800

    ✨ Added backbone routes and pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,67 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import DevIndexPage from "@/views/DevIndexPage.vue";
 
+// All routes outlined now are temporary for development
+// Future will sort the routes for the project
 const routes: Array<RouteRecordRaw> = [
+  // Dev index: Program initial sitemap
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "devindex",
+    component: DevIndexPage,
   },
+  // Landing page: The page where user first landed
   {
-    path: '/about',
-    name: 'About',
+    path: "/landing",
+    name: "landing",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "landing" */ "../views/LandingPage.vue"),
+  },
+  // Home page: Page where user logged in landed
+  {
+    path: "/home",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "../views/HomePage.vue"),
+  },
+  // Profile page: Page showing user details and posts
+  {
+    path: "/profile",
+    name: "profile",
+    component: () =>
+      import(/* webpackChunkName: "profile" */ "../views/ProfilePage.vue"),
+  },
+  // Settings page: Page for logged in users to manage their settings
+  {
+    path: "/settings",
+    name: "settings",
+    component: () =>
+      import(/* webpackChunkName: "settings" */ "../views/SettingsPage.vue"),
+  },
+  // Search result page: Page showing search results
+  {
+    path: "/searchresult",
+    name: "searchresult",
+    component: () =>
+      import(
+        /* webpackChunkName: "searchresult" */ "../views/SearchResultPage.vue"
+      ),
+  },
+  // Register page: Page showing registration form
+  {
+    path: "/register",
+    name: "register",
+    component: () =>
+      import(/* webpackChunkName: "register" */ "../views/RegisterPage.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;

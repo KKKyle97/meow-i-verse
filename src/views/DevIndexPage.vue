16e78b4994a5355c630ee01917047b8cf2987430
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-23 16:44:11 +0800

    ♻️ Migrate nav to devindex

--- src/views/DevIndexPage.vue
+++ src/views/DevIndexPage.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an devindex page</h1>
+    <nav id="nav">
+      <router-link :to="{ name: 'landing' }">Landing Page</router-link> |
+      <router-link :to="{ name: 'register' }">Register Page</router-link> |
+      <router-link :to="{ name: 'home' }">Home Page</router-link> |
+      <router-link :to="{ name: 'searchresult' }">Search Page</router-link> |
+      <router-link :to="{ name: 'chatlist' }">Chat Page</router-link> |
+      <router-link :to="{ name: 'settings' }">Settings Page</router-link> |
+      <router-link :to="{ name: 'profile' }">Profile Page</router-link>
+    </nav>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>

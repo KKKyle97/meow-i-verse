7ded282e10f2e8c311b867d295f3fd82d58674e4
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-06 16:55:35 +0800

    ✨ Implement layout in chatlist page

--- src/views/ChatListPage.vue
+++ src/views/ChatListPage.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an chatlist page</h1>
-  </div>
+  <DefaultLayout>
+    <div class="about">
+      <h1>This is an chatlist page</h1>
+    </div>
+  </DefaultLayout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import DefaultLayout from "@/layouts/Default.vue";
+
+export default defineComponent({
+  components: {
+    DefaultLayout,
+  },
+});
+</script>

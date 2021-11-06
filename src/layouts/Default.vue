b8ce9374814e0defedc772c49fdbd6d92a4764c8
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-06 16:54:47 +0800

    ✨ Added default layout component

---
+++ src/layouts/Default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="default">
+    <Appbar />
+    <main class="default__content">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Appbar from "@/components/_shared/Appbar.vue";
+
+export default defineComponent({
+  components: {
+    Appbar,
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.default
+  &__content
+    padding-top: 54px
+</style>

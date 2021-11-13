afb06f7fb67fdb49af1bc85e05d19620850403b3
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-13 20:58:46 +0800

    🚧 Added initial chat page layout

--- src/views/ChatListPage.vue
+++ src/views/ChatListPage.vue
@@ -1,7 +1,7 @@
 <template>
   <DefaultLayout>
-    <div class="about">
-      <h1>This is an chatlist page</h1>
+    <div class="chatlist-page">
+      <div class="chatlist-container"></div>
     </div>
   </DefaultLayout>
 </template>
@@ -16,3 +16,21 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="sass" scoped>
+@import "../assets/sass/_colors"
+
+.chatlist-page
+  padding: 20px
+  height: calc(100vh - 54px)
+
+  .chatlist-container
+    max-width: 935px
+    margin: 0 auto
+    width: 100%
+    height: 100%
+    background: $white
+    border: 1px solid $gray-200
+    overflow: auto
+    border-radius: 5px
+</style>
